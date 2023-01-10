9e6157fd68b633397218b30e97f285f8a162d8e0
kimberly3388 <kimberly.jaz@gmail.com>
2023-01-10 01:34:32 +0100

    Failure tolerance

--- services/src/main/java/si/fri/rso/ocena/services/beans/RatingBean.java
+++ services/src/main/java/si/fri/rso/ocena/services/beans/RatingBean.java
@@ -2,24 +2,49 @@ package si.fri.rso.ocena.services.beans;
 
 import com.kumuluz.ee.rest.beans.QueryParameters;
 import com.kumuluz.ee.rest.utils.JPAUtils;
+import org.eclipse.microprofile.faulttolerance.CircuitBreaker;
+import org.eclipse.microprofile.faulttolerance.Fallback;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import si.fri.rso.ocena.lib.Rating;
 import si.fri.rso.ocena.models.converters.RatingConverter;
 import si.fri.rso.ocena.models.entities.RatingEntity;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.NotFoundException;
+import javax.ws.rs.ProcessingException;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.GenericType;
 import javax.ws.rs.core.UriInfo;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
+import java.util.logging.Logger;
 import java.util.stream.Collectors;
 
 @RequestScoped
 public class RatingBean {
 
+    private Logger log = Logger.getLogger(RatingBean.class.getName());
+    @Inject
+    private RatingBean ratingBeanProxy;
+
     @Inject
     private EntityManager em;
+
+    private Client httpClient;
+    private String baseUrl;
+
+    @PostConstruct
+    private void init() {
+        httpClient = ClientBuilder.newClient();
+        baseUrl = "http://localhost:8082"; // only for demonstration
+    }
 
     public List<Rating> getRatings() {
         TypedQuery<RatingEntity> query = em.createNamedQuery("RatingEntity.getAll", RatingEntity.class);
@@ -47,6 +72,7 @@ public class RatingBean {
 
         Rating rating = RatingConverter.toDto(ratingEntity);
 
+        ratingBeanProxy.getDelivererInfo(rating.getDeliveryPerson());
         return rating;
     }
 
@@ -114,6 +140,31 @@ public class RatingBean {
         }
 
         return true;
+    }
+
+
+    //Klic druge mikrostoritve za napake in simulacijo tolerance napak, namenjeno za demonstracijo
+    @Timeout(value = 2, unit = ChronoUnit.SECONDS)
+    @CircuitBreaker(requestVolumeThreshold = 3)
+    @Fallback(fallbackMethod = "getFallback")
+    public Integer getDelivererInfo(Integer userId) {
+        log.info("Calling delivery-person service: getting their id to add to count.");
+
+        try {
+            return httpClient
+                    .target(baseUrl + "v1/dostavljalec/" + String.valueOf(userId))
+                    .request().get(new GenericType<Integer>() {
+                    });
+
+        } catch (WebApplicationException | ProcessingException e) {
+            log.severe(e.getMessage());
+            throw new InternalServerErrorException(e);
+        }
+    }
+
+    public Integer getFallback(Integer userId) {
+        log.info("Fallback.");
+        return null;
     }
 
     private void beginTx() {
